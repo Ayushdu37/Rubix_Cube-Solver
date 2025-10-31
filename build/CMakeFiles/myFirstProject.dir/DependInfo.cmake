68e6c9aa4cd6899e18eaba0b434d468caacf9785
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2025-10-31 20:12:14 +0530

    Add files via upload

---
+++ build/CMakeFiles/myFirstProject.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/Model/RubiksCube.cpp" "CMakeFiles/myFirstProject.dir/Model/RubiksCube.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/Model/RubiksCube.cpp.o.d"
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/Model/RubiksCube1dArray.cpp" "CMakeFiles/myFirstProject.dir/Model/RubiksCube1dArray.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/Model/RubiksCube1dArray.cpp.o.d"
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/Model/RubiksCube3dArray.cpp" "CMakeFiles/myFirstProject.dir/Model/RubiksCube3dArray.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/Model/RubiksCube3dArray.cpp.o.d"
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/Model/RubiksCubeBitBoard.cpp" "CMakeFiles/myFirstProject.dir/Model/RubiksCubeBitBoard.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/Model/RubiksCubeBitBoard.cpp.o.d"
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/PatternDatabases/CornerDBMaker.cpp" "CMakeFiles/myFirstProject.dir/PatternDatabases/CornerDBMaker.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/PatternDatabases/CornerDBMaker.cpp.o.d"
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/PatternDatabases/CornerPatternDatabase.cpp" "CMakeFiles/myFirstProject.dir/PatternDatabases/CornerPatternDatabase.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/PatternDatabases/CornerPatternDatabase.cpp.o.d"
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/PatternDatabases/NibbleArray.cpp" "CMakeFiles/myFirstProject.dir/PatternDatabases/NibbleArray.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/PatternDatabases/NibbleArray.cpp.o.d"
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/PatternDatabases/PatternDatabase.cpp" "CMakeFiles/myFirstProject.dir/PatternDatabases/PatternDatabase.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/PatternDatabases/PatternDatabase.cpp.o.d"
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/PatternDatabases/math.cpp" "CMakeFiles/myFirstProject.dir/PatternDatabases/math.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/PatternDatabases/math.cpp.o.d"
+  "/Users/vikalpshakya/Desktop/RubiksCubeSolver-main/main.cpp" "CMakeFiles/myFirstProject.dir/main.cpp.o" "gcc" "CMakeFiles/myFirstProject.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/myFirstProject.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube.cpp.o"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube1dArray.cpp.o"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube1dArray.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube3dArray.cpp.o"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube3dArray.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCubeBitBoard.cpp.o"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCubeBitBoard.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/CornerDBMaker.cpp.o"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/CornerDBMaker.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/CornerPatternDatabase.cpp.o"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/CornerPatternDatabase.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/NibbleArray.cpp.o"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/NibbleArray.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/PatternDatabase.cpp.o"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/PatternDatabase.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/math.cpp.o"
+  "CMakeFiles/myFirstProject.dir/PatternDatabases/math.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/main.cpp.o"
+  "CMakeFiles/myFirstProject.dir/main.cpp.o.d"
+  "myFirstProject"
+  "myFirstProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myFirstProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
