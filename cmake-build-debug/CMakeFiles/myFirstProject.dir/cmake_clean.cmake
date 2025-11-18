6dfbd076b9abcd6f53ac1bc674dd88b647127569
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2025-11-18 22:57:15 +0530

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/myFirstProject.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube.cpp.o"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube1dArray.cpp.o"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube1dArray.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube3dArray.cpp.o"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCube3dArray.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCubeBitBoard.cpp.o"
+  "CMakeFiles/myFirstProject.dir/Model/RubiksCubeBitBoard.cpp.o.d"
+  "CMakeFiles/myFirstProject.dir/main.cpp.o"
+  "CMakeFiles/myFirstProject.dir/main.cpp.o.d"
+  "myFirstProject"
+  "myFirstProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myFirstProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/myFirstProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for myFirstProject.
+# This may be replaced when dependencies are built.
